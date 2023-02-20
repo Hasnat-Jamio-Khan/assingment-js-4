0fd42a7a6b994613a0c5d8f4818788fa07d431ae
Hasnat <jamikhan527@gmail.com>
2023-02-20 21:22:11 +0600

    problems3-5 solve added

--- calculation.js
+++ calculation.js
@@ -40,61 +40,57 @@ function evenOdd (inputString) {
 // Function Explain : If substract the input number by 7 and check if the result is less than 7.
 function isLGSeven (inputNumber) {
 
-  if(typeof inputNumber === 'number'){
-    let difference = inputNumber - 7
-    if(difference < 7){
-        return difference;
+    if(typeof inputNumber === 'number'){
+      let difference = inputNumber - 7
+      if(difference < 7){
+          return difference;
+      }
+      else{
+          return  inputNumber * 2
+      }
     }
-    else{
-        return  inputNumber * 2
-    }
+    return 'Please give the valid input'
+  
   }
-  return 'Please give the valid input'
-
-}
-
-
-
-// Problem no :4
-// Function Explain : If find the bad data count in the input array of number. It is return array of number.
-
-function findingBadData (inputArrayOfNum) {
-
-    if (!Array.isArray(inputArrayOfNum) || inputArrayOfNum.length === 0) {
-        return "Input is not an array, Please provide an array";
-    }
-
-    let badDataCount = 0;
-    for (let i = 0; i < inputArrayOfNum.length; i++) {
-        if (inputArrayOfNum[i] < 0) {
-            badDataCount++;
-        }
-    }
-    return badDataCount;
-}
-
-
-
-
-
-// Problem no :5
-// Function Explain : If multliply firstFirendsGem if the input is not a number or empty, return alert.
-function gemsToDiamond (firstFirendsGem, secondFriendsGem, thirdFriendsGem) {
-
-    if (isNaN(firstFirendsGem) || !firstFirendsGem || isNaN(secondFriendsGem) || !secondFriendsGem || isNaN(thirdFriendsGem) || !thirdFriendsGem) {
-        return "Input is not a number, Please provide a number";
-    }
-    
-    const firstGem = firstFirendsGem * 21;
-    const secondGem = secondFriendsGem * 32;
-    const thirdGem = thirdFriendsGem * 43;
-
-    const total = firstGem + secondGem + thirdGem;
-
-    if (total >= 1000 * 2) {
-        return total - 2000;
-    } else {
-        return total;
-    }
-    
-}
+  
+  
+  // Problem no :4
+  // Function Explain : If find the bad data count in the input array of number. It is return array of number.
+  
+  function findingBadData (inputArrayOfNum) {
+  
+      if (!Array.isArray(inputArrayOfNum) || inputArrayOfNum.length === 0) {
+          return "Input is not an array, Please provide an array";
+      }
+  
+      let badDataCount = 0;
+      for (let i = 0; i < inputArrayOfNum.length; i++) {
+          if (inputArrayOfNum[i] < 0) {
+              badDataCount++;
+          }
+      }
+      return badDataCount;
+  }
+  
+  
+  // Problem no :5
+  // Function Explain : If multliply firstFirendsGem if the input is not a number or empty, return alert.
+  function gemsToDiamond (firstFirendsGem, secondFriendsGem, thirdFriendsGem) {
+  
+      if (isNaN(firstFirendsGem) || !firstFirendsGem || isNaN(secondFriendsGem) || !secondFriendsGem || isNaN(thirdFriendsGem) || !thirdFriendsGem) {
+          return "Input is not a number, Please provide a number";
+      }
+      
+      const firstGem = firstFirendsGem * 21;
+      const secondGem = secondFriendsGem * 32;
+      const thirdGem = thirdFriendsGem * 43;
+  
+      const total = firstGem + secondGem + thirdGem;
+  
+      if (total >= 1000 * 2) {
+          return total - 2000;
+      } else {
+          return total;
+      }
+      
+  }
